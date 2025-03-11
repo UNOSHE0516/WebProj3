3560dfae35d7bbf1bb16a98f03bd0f9588d5a742
UNOSHI0516 <hiro0516jms@gmail.com>
2025-03-11 21:24:35 +0900

    hello struts dto作 成

---
+++ src/com/diworksdev/webproj3/dto/HelloStrutsDTO.java
@@ -0,0 +1,14 @@
+package com.diworksdev.webproj3.dto;
+
+public class HelloStrutsDTO {
+	private String result;
+
+
+	public String getResult(){
+		return result;
+	}
+
+	public void serResult(String result){
+		this.result = result;
+	}
+}
